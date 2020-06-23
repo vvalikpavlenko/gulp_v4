e0bcbc01eb1d4d79d0a73813d37ceee491100897
vvalik <ghost0962421083>
2020-06-23 09:06:29 +0300

    23.06.2020

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,6 +1,6 @@
 module.exports =()=>{
     $.gulp.task('scripts:lib', ()=>{
-        return $.gulp.src(['node_modules/jquery/dist/jquery.min.js','./node_modules/slick-carousel/slick/slick.min.js'])// библиотеки
+        return $.gulp.src(['node_modules/jquery/dist/jquery.min.js',])// библиотеки
             .pipe($.gp.concat('libs.min.js'))
             .pipe($.gulp.dest('build/static/js/'))
             .pipe($.bs.reload({
